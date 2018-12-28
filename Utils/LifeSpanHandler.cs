de9eb05ed2b9e588562abc1a67cc17fedf2c624a
Setreuid <setreuid@naver.com>
2018-12-28 11:58:16 +0900

    New handler for disable popup.

---
+++ Utils/LifeSpanHandler.cs
@@ -0,0 +1,33 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEFParser.Utils
+{
+    public class LifeSpanHandler : ILifeSpanHandler
+    {
+        public bool DoClose(IWebBrowser browserControl, IBrowser browser)
+        {
+            return false;
+        }
+
+        public void OnAfterCreated(IWebBrowser browserControl, IBrowser browser)
+        {
+            // DO NOTHING
+        }
+
+        public void OnBeforeClose(IWebBrowser browserControl, IBrowser browser)
+        {
+            // DO NOTHING
+        }
+        
+        public bool OnBeforePopup(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, string targetUrl, string targetFrameName, WindowOpenDisposition targetDisposition, bool userGesture, IPopupFeatures popupFeatures, IWindowInfo windowInfo, IBrowserSettings browserSettings, ref bool noJavascriptAccess, out IWebBrowser newBrowser)
+        {
+            newBrowser = null;
+            return true;
+        }
+    }
+}
